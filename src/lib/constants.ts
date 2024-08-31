f924e02a41b987c69ce9e4d1e3ea01c07ba527b6
joeBlockchain <94577719+joeBlockchain@users.noreply.github.com>
2024-08-30 22:23:21 -0700

    initial commit

---
+++ src/lib/constants.ts
@@ -0,0 +1,64 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "News Applied AI | AI-Powered Newsletter",
+  description:
+    "Stay informed with our AI-curated newsletter, delivering the latest insights and applications of artificial intelligence across various industries.",
+  keywords: [
+    "AI newsletter",
+    "artificial intelligence news",
+    "applied AI",
+    "tech innovations",
+    "AI applications",
+  ],
+  authors: [{ name: "News Applied AI Team" }],
+  openGraph: {
+    title: "News Applied AI: Your Source for AI Innovations",
+    description:
+      "Discover how AI is transforming industries with our curated newsletter. Get the latest insights on applied artificial intelligence.",
+    url: "https://newsappliedai.com",
+    siteName: "News Applied AI",
+    images: [
+      {
+        url: "https://newsappliedai.com/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "News Applied AI Newsletter Preview",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "News Applied AI: AI Innovations at Your Fingertips",
+    description:
+      "Stay ahead of the curve with our AI-powered newsletter, bringing you the latest in applied artificial intelligence.",
+    creator: "@newsappliedai",
+    images: ["https://newsappliedai.com/twitter-image.jpg"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    // apple: '/apple-touch-icon.png',
+  },
+  //   verification: {
+  //     google: 'your-google-site-verification-code',
+  //     yandex: 'your-yandex-verification-code',
+  //     bing: 'your-bing-verification-code',
+  //   },
+  other: {
+    "application-name": "News Applied AI",
+  },
+};
